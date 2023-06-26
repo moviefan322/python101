4c87c37e53456efec67b282d52d48c0106d5082c
moviefan322 <philman202@yahoo.com>
2023-06-26 19:19:00 -0400

    adds operators file

--- strings.py
+++ strings.py
@@ -4,6 +4,17 @@ print("python's strings are easy to use")
 print('we can even include "quotes" in strings')
 print("hello" + " world")
 greeting = "hello"
-name = input("Please enter your name ")
+name = "Phil"
 
 print(greeting + ' ' + name)
+
+age = 24
+
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(age)
+print("age", type(age))
