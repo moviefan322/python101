357a65dca42638a9a96825938cf3dac3325d639a
moviefan322 <philman202@yahoo.com>
2023-06-26 14:30:29 -0400

    first commit

---
+++ strings.py
@@ -0,0 +1,9 @@
+print("today is a good day to learn python")
+print("python is fun")
+print("python's strings are easy to use")
+print('we can even include "quotes" in strings')
+print("hello" + " world")
+greeting = "hello"
+name = input("Please enter your name ")
+
+print(greeting + ' ' + name)
