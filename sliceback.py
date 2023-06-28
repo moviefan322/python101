d4ceffa5fbe2ee534f11636467fd3a5344ed3d9e
moviefan322 <philman202@yahoo.com>
2023-06-28 10:35:17 -0400

    adds sequence_operators examples

---
+++ sliceback.py
@@ -0,0 +1,11 @@
+letters = ""
+backwards = letters[25::-1]
+print(backwards)
+print(letters[-10:-13:-1])
+print(letters[4::-1])
+print(letters[:-9:-1])
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
--- strings2.py
+++ strings2.py
@@ -2,11 +2,13 @@
 parrot = "Norwegian Blue"
 
 print(parrot[0:6])  # Norweg
-print(parrot[3:5])  # we
-print(parrot[0:9])
-print(parrot[:9])
-print(parrot[10:])
-print(parrot[10:14])
 
-print(parrot[:6] + parrot[6:])
-print(parrot[:])
+print(parrot[0:6:2])  # Nre
+print(parrot[0:6:3])  # Nw
+
+number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
