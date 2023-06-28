6a1677f9f25b2d8aa29e599631b503c9e78594de
moviefan322 <philman202@yahoo.com>
2023-06-28 11:02:31 -0400

    adds examples for f strings

--- strings.py
+++ strings.py
@@ -16,5 +16,9 @@ print(type(greeting))
 print(type(age))
 
 age = "2 years"
-print(age)
+print(name + f" is {age} years old")
 print("age", type(age))
+
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
